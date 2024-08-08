40f9cdee594f732d98b233ff330ec8259e90e6d7
kushal1997 <kushalrao103@gmail.com>
2024-08-08 17:08:27 +0530

    first commit

---
+++ first_test.ts
@@ -0,0 +1,54 @@
+let userName:string
+
+// userName=34;
+userName='Max';
+
+
+let userId:string | number ='jasja';
+
+userId=23;
+
+
+//object
+let user:{
+name:string;
+age:number;
+isAdmin:boolean;
+id:string|number;
+};
+
+user={
+    name:'kushal',
+    age:27,
+    isAdmin:true,
+    id:545
+}
+
+
+// array
+
+// let cars:Array<string>;
+let cars:string[]
+
+cars=['maruti','mg','nano'];
+
+let userData:{
+    name:string;
+    age:number;
+    isAdmin:boolean;
+    id:string|number;
+    }[];
+
+    userData=[{
+        name:'kushal',
+        age:27,
+        isAdmin:true,
+        id:545
+    },
+    {
+        name:'kushal',
+        age:27,
+        isAdmin:true,
+        id:545
+    }
+]
