b68983c61681f62b9b7c85dbc712504128c9735d
kushal1997 <kushalrao103@gmail.com>
2024-08-09 00:32:15 +0530

    from form- add,data,, delete data done

--- 2_react-ts-basics/src/App.tsx
+++ 2_react-ts-basics/src/App.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
+import CourseGoalList from "./components/CourseGoalList";
+import { formType, type CourseGoal as CourseGoalType } from "./types";
+import NewGoal from "./components/NewGoal";
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  //if we are going to use useState, then
+  //we need to define its type as useSate is a generic type
+  // we can pass the custom types within the brackets <>
+
+  const [goals, setGoals] = useState<CourseGoalType[]>([]);
+
+  const handleAddGoal = (values:formType) => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoalType = {
+        id: Math.random(),
+        title: values.goal,
+        description: values.summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
+
+  const handleDeleteGoal = (id: number) => {
+    setGoals((prevGoals) => prevGoals?.filter((el) => el.id !== id));
+  };
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      {/* <button onClick={handleAddGoal}>Click here</button> */}
+      <NewGoal addGoalFunc={handleAddGoal}/>
+      <CourseGoalList goals={goals} onDelete={handleDeleteGoal} />
+    </main>
+  );
 }
---
+++ 2_react-ts-basics/src/types/index.ts
@@ -0,0 +1,3 @@
+
+export * from "./headerTypes"
+export * from "./courseTypes"
