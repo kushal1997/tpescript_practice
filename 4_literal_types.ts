16d8525be160fb8cf1045ec8dbd7798df864c0be
kushal1997 <kushalrao103@gmail.com>
2024-08-08 17:41:08 +0530

    type guards & type narrowing

--- 4_literal_types.ts
+++ 4_literal_types.ts
@@ -15,3 +15,56 @@ role='recruiter';
 //apart from the above three declared as template literals
 
 // role='abc'
+
+
+//=======================================================================================================
+
+//adding type guards
+
+type Role='admin'|'user'|'recruiter';
+
+function doAction(action:string|number,role:Role){
+    if(role==='admin' && typeof action==='string') { 
+        //...do some logic
+    }
+}
+
+
+//=======================================================================================================
+
+//type narrowing
+
+function combine(a: number | string, b: number | string) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a + b;
+    }
+   
+    return `${a} ${b}`;
+  }
+
+
+  type User1 = {
+    name: string;
+    age: number;
+  };
+   
+  type Admin1 = {
+    name: string;
+    age: number;
+    permissions: string[];
+  };
+   
+//   the below code won't work because the User type does not exist once the code is compiled to JavaScript:
+//   function login(u: User1 | Admin1) {
+//     if (typeof u === User1) {
+//       // do something
+//     }
+//   }
+
+
+// That code would work at runtime.
+function login1(u: User1 | Admin1) {
+    if ('permissions' in u) {
+      // do something
+    }
+  }
