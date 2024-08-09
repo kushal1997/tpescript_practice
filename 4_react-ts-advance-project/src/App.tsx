c0de5a53ec257906a6aa694bff7bcb57c324bb70
kushal1997 <kushalrao103@gmail.com>
2024-08-09 11:15:16 +0530

    wrapper component

--- 4_react-ts-advance-project/src/App.tsx
+++ 4_react-ts-advance-project/src/App.tsx
@@ -1,5 +1,20 @@
+import Input from "./components/Input";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <>
+      <main>
+        <Input id="name" label="Your name" />
+        <Input
+          id="age"
+          label="your age"
+          type="number"
+          placeholder="enter age"
+          autoFocus
+        />
+      </main>
+    </>
+  );
 }
 
 export default App;
